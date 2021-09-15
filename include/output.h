dafafea7f7663e908147f23a05327184913b7dd4
Davide Cavalca <dcavalca@fb.com>
2021-09-15 08:15:14 -0700

    Initial import
    Initial import of the netconsd and ncrx codebases from
    https://github.com/facebook/fbkutils

---
+++ include/output.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+#ifndef __OUTPUT_H__
+#define __OUTPUT_H__
+
+#include <ncrx-struct.h>
+
+#include "msgbuf-struct.h"
+
+#define MAXOUTS 32
+
+int register_output_module(char *path, int nr_workers);
+void destroy_output_modules(void);
+
+void execute_output_pipeline(int thread_nr, struct in6_addr *src,
+		struct msg_buf *buf, struct ncrx_msg *msg);
+
+#endif /* __OUTPUT_H__ */
---
+++ include/threads.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+#ifndef __NCRX_THREADS_H__
+#define __NCRX_THREADS_H__
+
+#include "msgbuf-struct.h"
+#include "common.h"
+
+struct tctl;
+struct ncrx_listener;
+
+void enqueue_and_wake_all(struct ncrx_listener *listener);
+struct tctl *create_threads(struct netconsd_params *p);
+void destroy_threads(struct tctl *ctl);
+
+#endif /* __NCRX_THREADS_H__ */
